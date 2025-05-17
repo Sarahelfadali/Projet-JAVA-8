f03dbfb6993fcea20acfb8e3f2368b8e84094fe1
Sarahelfadali <92674026+Sarahelfadali@users.noreply.github.com>
2025-05-17 11:59:27 +0200

    Update Room.java

--- src/main/java/org/house/projetjava8/model/Room.java
+++ src/main/java/org/house/projetjava8/model/Room.java
@@ -4,8 +4,18 @@ import java.util.List;
 public class Room {
     private int id;
     private String name;
-    private List<Bed> beds;
     private String genderRestriction;
-    private int minAge;
-    private int maxAge;
+    private Integer minAge;
+    private Integer maxAge;
+    private List<Bed> beds;
+
+    public Room(int id, String name, String genderRestriction, Integer minAge, Integer maxAge, List<Bed> beds) {
+        this.id = id;
+        this.name = name;
+        this.genderRestriction = genderRestriction;
+        this.minAge = minAge;
+        this.maxAge = maxAge;
+        this.beds = beds;
+    }
+
 }
