581cdbbbc7a7bf96ec8b0236f16832b068bcf677
Sarahelfadali <92674026+Sarahelfadali@users.noreply.github.com>
2025-05-17 12:00:22 +0200

    Update Bed.java

--- src/main/java/org/house/projetjava8/model/Bed.java
+++ src/main/java/org/house/projetjava8/model/Bed.java
@@ -2,6 +2,17 @@ package org.house.projetjava8.model;
 
 public class Bed {
     private int id;
-    private String label;
+    private String name;
     private int capacity;
+    private int roomId;
+
+
+    public Bed(int id, String name, int capacity, int roomId) {
+        this.id = id;
+        this.name = name;
+        this.capacity = capacity;
+        this.roomId = roomId;
+    }
+
+
 }
