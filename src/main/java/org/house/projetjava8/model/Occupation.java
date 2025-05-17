ddbb1817f0502d6e6247bf3a212b07de1b4e5486
Sarahelfadali <92674026+Sarahelfadali@users.noreply.github.com>
2025-05-17 12:01:20 +0200

    Update Occupation.java

--- src/main/java/org/house/projetjava8/model/Occupation.java
+++ src/main/java/org/house/projetjava8/model/Occupation.java
@@ -1,11 +1,18 @@
 package org.house.projetjava8.model;
 import java.time.LocalDate;
-
 public class Occupation {
     private int id;
-    private Person person;
-    private Bed bed;
-    private LocalDate startDate;
-    private LocalDate endDate;
-    private boolean hasLeft;
+    private int personId;
+    private int bedId;
+    private String startDate;
+    private String endDate;
+
+    public Occupation(int id, int personId, int bedId, String startDate, String endDate, boolean exited) {
+        this.id = id;
+        this.personId = personId;
+        this.bedId = bedId;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.exited = exited;
+    }
 }
