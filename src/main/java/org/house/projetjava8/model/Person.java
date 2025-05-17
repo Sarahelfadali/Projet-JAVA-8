227a2f112ea4c236fd7e3815885398ea30ad9167
Sarahelfadali <92674026+Sarahelfadali@users.noreply.github.com>
2025-05-17 11:58:05 +0200

    Update Person.java

--- src/main/java/org/house/projetjava8/model/Person.java
+++ src/main/java/org/house/projetjava8/model/Person.java
@@ -2,14 +2,31 @@ package org.house.projetjava8.model;
 import java.time.LocalDate;
 import java.util.List;
 
+
 public class Person {
     private int id;
     private String lastName;
     private String firstName;
     private String gender;
-    private LocalDate birthDate;
+    private String birthDate;
     private String birthCity;
     private String socialSecurityNumber;
-    private List<String> addresses;
+    private List<String> knownAddresses;
+
     
+
+    public Person(int id, String lastName, String firstName, String gender, String birthDate,
+                  String birthCity, String socialSecurityNumber, List<String> knownAddresses) {
+        this.id = id;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        this.gender = gender;
+        this.birthDate = birthDate;
+        this.birthCity = birthCity;
+        this.socialSecurityNumber = socialSecurityNumber;
+        this.knownAddresses = knownAddresses;
+    }
+
+    
+
 }
